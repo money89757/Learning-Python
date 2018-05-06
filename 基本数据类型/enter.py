08b1471751d4bb5785d9d36cfa9eca5389c91b5f
money89757 <money89757757@163.com>
2018-05-06 16:21:57 +0800

    demo

---
+++ 基本数据类型/enter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-05-06
+# @Author  : money89757757@163.com
+
+import os
+
+# input("\n\n按下 enter 后推出")
+
+import sys; x = 'runoob';sys.stdout.write (x + '\n')
