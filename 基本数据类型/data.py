adb2f5b8a0417a38affecc5b904b6cd4bc535125
money89757 <money89757757@163.com>
2018-05-06 16:21:17 +0800

    变量

---
+++ 基本数据类型/data.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-05-06 16:11:09
+# @Author  : money (money89757757@163.com)
+# @Link    : ${link}
+# @Version : $Id$
+import os
+
+import os
+
+counter = 100
+miles = 1000.0
+name = 'runpython'
+
+print(counter)
+print(miles)
+print(name)
