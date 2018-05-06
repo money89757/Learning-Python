253bbcf71649f41114add9ffef37d30c84749edb
money89757 <money89757757@163.com>
2018-05-06 16:22:46 +0800

    字符串

---
+++ 基本数据类型/string.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Author  : money89757757@163.com
+# @Date    : 2018-05-06
+#!/usr/bin/env python
+import os
+
+int str ='Runnoob'
+print('----------------')
+print('hello\nrunoob')      # 使用反斜杠(\)+n转义特殊字符
+print(r'hello\nrunoob')     # 在字符串前面添加一个 r，表示原始字符串，不会发生转义
+print(str)             #输出字符串
+print(str+'你好')      #字符添加
+print(str[0:-1])	   #输出第一个和倒数第二个的所有字符
+print(str[0])          #输出第一个
+print(str[2:5])        #输出第二个和倒数第五个的所有字符
+print(str[2:])         #输出第二个以后的字符
+str='Runnoob'
